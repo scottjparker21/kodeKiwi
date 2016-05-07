f83b08367e6cbbef890e9d15aa016918fb329510
scottjparker21 <scottjparker21@gmail.com>
2016-05-07 13:58:22 -0500

    trying to get server to load.

--- index.js
+++ index.js
@@ -16,13 +16,37 @@ var options = {
   'commmitter':{'name': author, 'email': email}
 };
 
+// http.listen(process.env.PORT || 3000, function(){
+//   console.log('listening on', http.address().port);
+// });
 
-server.listen(port, function () {
-  console.log('Server listening at port %d', port);
-});
+//trouble shooting heroku server here ---------->
+app.set('port', (process.env.PORT || 5000));
 
 // Routing
 app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+
+//end heroku server trouble shooting ----------->
+
+// server.listen(port, function () {
+//   console.log('Server listening at port %d', port);
+// });
+
+
 
 // Chatroom
 
